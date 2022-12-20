0b45ec740c71e92094b22a50faa330301b1e97b0
Sandy <saritasuper9211@gmail.com>
2022-12-20 12:58:38 +0530

    add faq

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,23 @@
-const nav = document.querySelector('.nav');
-const navToggle = document.querySelector('.nav-toggle');
-const layer = document.querySelector('.layer');
+const nav = document.querySelector(".nav");
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll('.nav__link');
+const layer = document.querySelector(".layer");
+const faqs = document.querySelectorAll(".faq__item");
+
+const toggleNav = function () {
+  nav.classList.toggle("active");
+  navToggle.classList.toggle("active");
+  layer.classList.toggle("active");
+};
+navToggle.addEventListener("click", toggleNav);
 
 
-const toggleNav = function() {
-    nav.classList.toggle('active');
-    navToggle.classList.toggle('active');
-    layer.classList.toggle('active');
+
+const hideNav = function() {
+  nav.classList.remove("active");
+  navToggle.classList.remove("active");
+  layer.classList.remove("active");
 }
-navToggle.addEventListener('click',toggleNav);
+for (const navLink of navLinks) {
+  navLink.addEventListener('click',hideNav);
+}
